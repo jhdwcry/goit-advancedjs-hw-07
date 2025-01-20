38eba2da99a8db92e6562afbd85394b0df46afa0
jhdwcry <korn.anastasia02@gmail.com>
2025-01-20 17:59:35 +0200

    done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,66 @@
-const key = new Key();
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature; 
+    }
+  }
+  
+  
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key; 
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  abstract class House {
+    protected door: boolean = false; 
+    protected key: Key; 
+    protected tenants: Person[] = []; 
+    constructor(key: Key) {
+      this.key = key; 
+    }
+  
+    abstract openDoor(key: Key): void; 
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person); 
+        console.log(`${person.getKey().getSignature()} has entered the house.`);
+      } else {
+        console.log("The door is closed. You can't come in.");
+      }
+    }
+  }
+  
 
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true; 
+        console.log('Door is now open!');
+      } else {
+        console.log('Wrong key! The door remains closed.');
+      }
+    }
+  }
+  
+  // Тестування сценарію
+  const key = new Key(); 
+  const house = new MyHouse(key);
+  const person = new Person(key); 
+  
+  house.openDoor(person.getKey()); 
+  house.comeIn(person); 
+  
